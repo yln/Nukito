14fa54660ead95f49354f0f688487de316decb91
Julian Lettner <julian.lettner@gmail.com>
2011-09-27 19:11:44 +0200

    Add failing test for MockBehavior configuration.

---
+++ Nukito.Test/Scenario/MockBehaviorScenarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+
+namespace Nukito.Test.Scenario
+{
+  public class MockBehaviorScenarios
+  {
+    [NukitoFact]
+    public void DefaultMockBehavior(Mock<IA> mock)
+    {
+      // Assert (Default -> Loose)
+      mock.Behavior.Should().Be(MockBehavior.Default).And.Be(MockBehavior.Loose);
+    }
+
+    [NukitoFact]
+    public void MockBehaviorCanBeConfigured(Mock<IA> mock)
+    {
+      // Assert
+      mock.Behavior.Should().Be(MockBehavior.Strict);
+      // TODO: find nice way to allow this
+    }
+
+    public class Bla
+    {
+      [NukitoFact]
+      public void Blub(Mock<IA> mock)
+      {
+        throw new Exception("fail");
+        // TODO: settings on class level
+      }
+    }
+  }
+}
