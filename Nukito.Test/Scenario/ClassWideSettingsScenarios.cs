daf02040c00312a2e9ef0ef5330e0f52d2195480
Julian Lettner <julian.lettner@gmail.com>
2011-09-29 16:12:38 +0200

    Add failing test for class wide settings

---
+++ Nukito.Test/Scenario/ClassWideSettingsScenarios.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using Moq;
+
+namespace Nukito.Test.Scenario
+{
+  // --- Default Settings ---
+  // MockBehavior -> Default (Loose)
+  [NukitoSettings(MockBehavior = MockBehavior.Strict)]
+  public class ClassWideSettingsScenarios
+  {
+    [NukitoFact]
+    public void ClassWideMockBehavior(Mock<IA> mock)
+    {
+      // Assert
+      mock.Behavior.Should().Be(MockBehavior.Strict);
+    }
+
+    [NukitoFact]
+    // TODO: add settings to override class wide settings
+    public void ReConfiguredMockBehavior(Mock<IA> mock)
+    {
+      // Assert
+      mock.Behavior.Should().Be(MockBehavior.Default).And.Be(MockBehavior.Loose);
+    }
+  }
+}
