28787c02c4301a3b9867c7a9ef9874116f9128d5
Julian Lettner <julian.lettner@gmail.com>
2011-10-01 16:47:47 +0200

    Add failing tests for mock verification and re-factorings
    Re-factorings include the handling of settings and TestUtility

--- Nukito.Test/Scenario/ClassWideSettingsScenarios.cs
+++ Nukito.Test/Scenario/ClassWideSettingsScenarios.cs
@@ -1,11 +1,13 @@
 ﻿using FluentAssertions;
 using Moq;
+using Nukito.Internal;
 
 namespace Nukito.Test.Scenario
 {
   // --- Default Settings ---
-  // MockBehavior -> Default (Loose)
-  [NukitoSettings(MockBehavior = MockBehavior.Strict)]
+  // MockBehavior -> Loose (and also 'Default')
+  // MockVerification -> All
+  [NukitoSettings(MockBehavior = MockBehavior.Strict, MockVerification = MockVerification.Marked)]
   public class ClassWideSettingsScenarios
   {
     [NukitoFact]
@@ -20,6 +22,26 @@ namespace Nukito.Test.Scenario
     {
       // Assert
       mock.Behavior.Should().Be(MockBehavior.Default).And.Be(MockBehavior.Loose);
+    }
+
+    [NukitoFact]
+    public void ClassWideMockVerification(Mock<IB> mock)
+    {
+      // Arrange
+      mock.Setup(b => b.DoSomething());
+
+      // Assert: no exception
+      // Implicit expectations (default overwritten by class-wide) are not fullfilled
+    }
+
+    [NukitoFact(MockVerification = MockVerification.None)]
+    public void ReConfiguredMockVerification(Mock<IB> mock)
+    {
+      // Arrange
+      mock.Setup(b => b.DoSomething()).Verifiable();
+
+      // Assert: no exception
+      // Marked expectations (class-wide overwritten by method) are not fullfilled
     }
   }
 }
