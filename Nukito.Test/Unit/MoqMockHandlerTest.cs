420aad8a9c850ac7520d28d3396cf9e787521b06
Julian Lettner <julian.lettner@gmail.com>
2011-10-02 13:15:27 +0200

    Finish implementation of MoqMockHandler.

---
+++ Nukito.Test/Unit/MoqMockHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Moq;
+using Nukito.Internal;
+using Nukito.Test.Scenario;
+using Nukito.Test.Utility;
+
+namespace Nukito.Test.Unit
+{
+  public class MoqMockHandlerTest
+  {
+    [NukitoFact]
+    public void CreateMock()
+    {
+      // Arrange
+      var mockHandler = new MoqMockHandler(new MockRepository(MockBehavior.Loose));
+
+      // Act
+      object result = mockHandler.CreateMock(typeof (IA));
+
+      // Assert
+      result.Should().BeMock<IA>();
+    }
+  }
+}
--- Nukito/Internal/MoqMockHandler.cs
+++ Nukito/Internal/MoqMockHandler.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Reflection;
 using Moq;
 
 namespace Nukito.Internal
 {
   internal class MoqMockHandler : IMockHandler
   {
+    private static readonly object _Locker = new object();
+    private static MethodInfo _createMethod;
+
+    private static MethodInfo CreateMethod
+    {
+      get
+      {
+        lock (_Locker)
+        {
+          return _createMethod ?? (_createMethod = typeof (MockRepository).GetMethod("Create", new Type[0]));
+        }
+      }
+    }
+
     private readonly MockRepository _mockRepository;
 
     public MoqMockHandler(MockRepository mockRepository)
@@ -14,7 +29,9 @@ namespace Nukito.Internal
 
     public object CreateMock(Type type)
     {
-      throw new NotImplementedException();
+      MethodInfo methodInfo = CreateMethod.MakeGenericMethod(type);
+      var mock = (Mock) methodInfo.Invoke(_mockRepository, new object[0]);
+      return mock.Object;
     }
 
     public void VerifyAll()
