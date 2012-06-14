d52d19067411dde7178c83ef327b2f28bc18901b
Julian Lettner <julian.lettner@gmail.com>
2012-06-15 01:56:20 +0200

    Add MockRepository example

---
+++ Nukito.Example/MockRepositoryExample.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Moq;
+
+namespace Nukito.Example
+{
+  public class MockRepositoryExample
+  {
+    [NukitoFact]
+    public void MockRepositoryCanBeRetrieved(MockRepository mockRepository)
+    {
+      // Assert
+      mockRepository.Should().NotBeNull();
+
+      // Do stuff with the repository ...
+    }
+  }
+}
