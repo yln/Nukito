9bda53e9b396c4464f97506b9ec17ca41576bf67
Julian Lettner <julian.lettner@gmail.com>
2012-06-16 16:14:01 +0200

    Update SettingsExample

--- Nukito.Example/SettingsExample.cs
+++ Nukito.Example/SettingsExample.cs
@@ -5,13 +5,26 @@ namespace Nukito.Example
 {
   // Mock settings may be configured on class level.
   // The following settings are the defaults.
-  [MockSettings(
+  [MockSettings (
     Behavior = MockBehavior.Loose,
     CallBase = false,
     DefaultValue = DefaultValue.Mock,
     Verification = MockVerification.All)]
   public class SettingsExample
   {
+    private readonly IWeapon _weapon;
+
+    // Mock settings on constructors overwrite class level settings.
+    // Usage of [MockSettings (Verification = ...)] is not allowed.
+    //[MockSettings (Behavior = MockBehavior.Strict, Verification = MockVerification.None)]
+    //public SettingsExample(Mock<IWeapon> weapon)
+    //{
+    // TODO: fix after 'injection' context has been implemented
+    //  // Common setup
+    //  weapon.Setup (w => w.Name).Returns("sword");
+    //  _weapon = weapon.Object;
+    //}
+
     [NukitoFact]
     public void MockSettingsDefaults(Mock<IWarrior> warrior)
     {
@@ -29,16 +42,16 @@ namespace Nukito.Example
     }
 
 
-    // Mock settings can be configured on method and class level.
+    // Mock setting on methods overwrite class level settings.
     [NukitoFact]
-    [MockSettings(Behavior = MockBehavior.Strict, Verification = MockVerification.None)]
+    [MockSettings (Behavior = MockBehavior.Strict, Verification = MockVerification.None)]
     public void MockSettingsCanBeConfigured(Mock<IWarrior> warrior)
     {
       // Arrange
       warrior.Setup(w => w.Fight()).Returns("Splat!");
 
       // Act
-      // This test still passes although we are not calling 'Fight()'.
+      // This test passes although 'Fight()' is not called.
 
       // Assert
       warrior.Behavior.Should().Be(MockBehavior.Strict);
